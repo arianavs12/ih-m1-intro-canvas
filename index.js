b3eb5d42ab7a92adb91a84afaf7a5e4b0825def9
Ariana <pilarsv@outlook.com>
2022-03-29 15:27:29 -0600

    Perimetro de del canvas listo

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hola")
+// index.js
+
+const canvas 	= document.getElementById("game")
+const ctx 		= canvas.getContext("2d")
+
+
+// RECTÁNGULO
+ctx.fillStyle = "purple"
+ctx.fillRect(260, 260, 30, 30)
